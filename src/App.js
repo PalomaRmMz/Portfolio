46f9463cfb70694f1f0a7cc5c2afa4547533b5f4
Paloma Ramírez Meza <p.ramirez.m.8080@gmail.com>
2021-03-18 10:11:52 -0600

    Create footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
       <h1>App</h1>
-    </div>
+      <Footer />
+    </>
   );
 }
 
