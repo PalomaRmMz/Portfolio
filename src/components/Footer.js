dc0ebedd7ced0096413e7ac81db94f3c32750dd6
Paloma Ramírez Meza <p.ramirez.m.8080@gmail.com>
2021-03-18 10:21:24 -0600

    Add some styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
 import React from "react";
+import "./Footer.css";
 
 class Footer extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Footer</h1>
-      </div>
+      <>
+        <footer className="Footer">
+          {/* <div className="footer"> */}
+          <p>
+            Designed with {/*<i class="fas fa-heart"></i>*/} by{" "}
+            <a href="https://github.com/PalomaRmMz" className="Navbar__link">
+              Paloma RM
+            </a>
+            {/* <i class="fas fa-copyright"></i> */}
+          </p>
+          {/* </div> */}
+        </footer>
+      </>
     );
   }
 }
